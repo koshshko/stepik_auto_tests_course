99c9bd497eb6d7342ce7689fca02a733a38f6ae3
Anna <75202228+koshshko@users.noreply.github.com>
2022-06-01 17:33:45 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,24 +1,20 @@
 import pytest
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', default=None)
+        parser.addoption('--browser_name', action='store', default=None,
+                         help="Choose browser: chrome or firefox")
+        parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: ")
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
+    user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart chrome browser for test..")
     browser = webdriver.Chrome(options=options)
-    print("\nstart browser for test..")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-@pytest.mark.parametrize('language', [user_language])
-def test_basket_button(browser, language):
-    link = f"https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    browser.get(link)
-    time.sleep(30)
